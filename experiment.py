324c3f09efac40b63c1462465f3cc4cf01318108
Loris Danjou <loris.danjou@meteo.fr>
2023-03-29 14:05:50 +0200

    première version data_loader

--- experiment.py
+++ experiment.py
@@ -4,6 +4,7 @@ import random as rn
 from bronx.stdtypes.date import daterangex as rangex
 from make_unet import *
 import matplotlib.pyplot as plt
+from data_loader import *
 
 data_train_location = '/cnrm/recyf/Data/users/danjoul/dataset/data_train/'
 data_valid_location = '/cnrm/recyf/Data/users/danjoul/dataset/data_test/'
@@ -30,59 +31,7 @@ size_2km5_crop = highestPowerof2(min(geometry_2km5))
 '''
 Loading data
 '''
-# initial shape of the data:
-    # X[date, ech, x, y, param]
-    # y[date, ech, x, y]
-X_train = np.zeros(shape=[len(dates_train), len(echeances), geometry_2km5[0], geometry_2km5[1], len(params)], dtype=np.float32)
-y_train = np.zeros(shape=[len(dates_train), len(echeances), geometry_500m[0], geometry_500m[1]], dtype=np.float32)
-X_valid = np.zeros(shape=[len(dates_valid), len(echeances), geometry_2km5[0], geometry_2km5[1], len(params)], dtype=np.float32)
-y_valid = np.zeros(shape=[len(dates_valid), len(echeances), geometry_500m[0], geometry_500m[1]], dtype=np.float32)
-
-for i_d, d in enumerate(dates_train):
-    try:
-        filepath_y_train = data_train_location + 'G9L1_' + d.isoformat() + 'Z_t2m.npy'
-        y_train[i_d, :, :, :] = np.load(filepath_y_train).transpose([2,0,1])
-    except :
-        filepath_y_train = data_train_location + 'G9KP_' + d.isoformat() + 'Z_t2m.npy'
-        y_train[i_d, :, :, :] = np.load(filepath_y_train).transpose([2,0,1])
-    for i_p, p in enumerate(params):
-        filepath_X_train = data_train_location + 'oper_c_' + d.isoformat() + 'Z_' + p + '.npy'
-        X_train[i_d, :, :, :, i_p] = np.load(filepath_X_train).transpose([2,0,1])
-
-for i_d, d in enumerate(dates_valid):
-    filepath_y_valid = data_valid_location + 'G9L1_' + d.isoformat() + 'Z_t2m.npy'
-    y_valid[i_d, :, :, :] = np.load(filepath_y_valid).transpose([2,0,1])
-    for i_p, p in enumerate(params):
-        filepath_X_valid = data_valid_location + 'oper_c_' + d.isoformat() + 'Z_' + p + '.npy'
-        X_valid[i_d, :, :, :, i_p] = np.load(filepath_X_valid).transpose([2,0,1])
-
-print(X_train.shape)
-print(y_train.shape)
-print(X_valid.shape)
-print(y_valid.shape)
-
-# final shape of the data : 
-    # X[date/ech, x, y, param]
-    # y[date/ech, x, y]
-X_train = X_train.reshape((-1, geometry_2km5[0], geometry_2km5[1], len(params)))
-y_train = y_train.reshape((-1, geometry_500m[0], geometry_500m[1]))
-X_valid = X_valid.reshape((-1, geometry_2km5[0], geometry_2km5[1], len(params)))
-y_valid = y_valid.reshape((-1, geometry_500m[0], geometry_500m[1]))
-
-print(X_train.shape)
-print(y_train.shape)
-print(X_valid.shape)
-print(y_valid.shape)
-
-X_train = X_train[:, int(0.5* (geometry_2km5[0] - size_2km5_crop)):int(0.5* (geometry_2km5[0] + size_2km5_crop)), int(0.5* (geometry_2km5[1] - size_2km5_crop)):int(0.5* (geometry_2km5[1] + size_2km5_crop)), :]
-y_train = y_train[:, int(0.5* (geometry_500m[0] - size_500m_crop)):int(0.5* (geometry_500m[0] + size_500m_crop)), int(0.5* (geometry_500m[1] - size_500m_crop)):int(0.5* (geometry_500m[1] + size_500m_crop))]
-X_valid = X_valid[:, int(0.5* (geometry_2km5[0] - size_2km5_crop)):int(0.5* (geometry_2km5[0] + size_2km5_crop)), int(0.5* (geometry_2km5[1] - size_2km5_crop)):int(0.5* (geometry_2km5[1] + size_2km5_crop)), :]
-y_valid = y_valid[:, int(0.5* (geometry_500m[0] - size_500m_crop)):int(0.5* (geometry_500m[0] + size_500m_crop)), int(0.5* (geometry_500m[1] - size_500m_crop)):int(0.5* (geometry_500m[1] + size_500m_crop))]
-
-print(X_train.shape)
-print(y_train.shape)
-print(X_valid.shape)
-print(y_valid.shape)
+X_train, y_train, X_valid, y_valid = load_data(dates_train, dates_valid, echeances, data_train_location, data_valid_location, params)
 
 
 '''
